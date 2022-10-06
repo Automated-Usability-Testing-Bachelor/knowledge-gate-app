42b86117fded7faaaddfe52b1e364ea67bdcaae8
ArditGashi <sejeardit@hotmail.com>
2022-10-06 14:03:09 +0200

    fix svg rendering locally

--- components/loginComponents/Logo.tsx
+++ components/loginComponents/Logo.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {Image, StyleSheet, View} from "react-native";
 
 const Logo = () => {
-    const icon = require("../../assets/logos/AdditionalLockups/Lockup02/whiteLogo.png");
+    const icon = require("../../assets/logos/Additional_Lockups/Lockup02/whiteLogo.png");
     return (
         <View style={styles.container}>
             <Image style={styles.logo} source={icon}/>
