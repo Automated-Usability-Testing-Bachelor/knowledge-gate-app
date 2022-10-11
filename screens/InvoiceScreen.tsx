b00555f950e82a75b51f9b9535e04d9099ef1361
ArditGashi <sejeardit@hotmail.com>
2022-10-11 12:16:29 +0200

    bump dep, add eslint, prettier

--- screens/InvoiceScreen.tsx
+++ screens/InvoiceScreen.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Text, View} from "react-native";
 import BackgroundTemplate from "../components/BackgroundTemplate";
 import InvoiceCards from "../components/invoiceComponents/InvoiceCards";
 
