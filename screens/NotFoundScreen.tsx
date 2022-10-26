82dafa62e439d8daa8e77e2d592241c2ae3bf5b7
Simon Karing <simonkaring@gmail.com>
2022-10-26 15:38:10 +0200

    Delete notfoundscreen

--- screens/NotFoundScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import BackgroundTemplate from '../components/BackgroundTemplate'
-import testComponent from '../components/TestComponent'
-
-const NotFoundScreen = () => {
-  return <BackgroundTemplate body={testComponent()} />
-}
-
-export default NotFoundScreen
