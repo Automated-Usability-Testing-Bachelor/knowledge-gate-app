837f046257aab30127caaac11961442d666d3158
Markus Munk <mmunk19@student.sdu.dk>
2022-10-26 11:23:12 +0200

    eslint fix on actualCalendar

--- components/calendarComponents/actualCalendar.tsx
+++ components/calendarComponents/actualCalendar.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Animated, Image, StyleSheet, View } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import CalendarPicker, {
   CustomDateStyle,
   CustomDayHeaderStylesFunc
@@ -10,134 +10,8 @@ import Colors from '../../constants/Colors'
 import { textStyles } from '../Texts/Headers'
 import FadeInView from '../animatedComponents/FadeInView'
 import { DateWithAvailability } from '../../mocks/CalendarMockData'
+import Logo from '../../assets/logos/Logomark/KGG_Logomark_Blue_RGB.svg'
 
-export type Props = {
-  datesWithAvailabilities: DateWithAvailability[]
-  selectedDayCallback: Function
-  currentMonthCallback: Function
-}
-
-const ActualCalendar: React.FC<Props> = ({
-  datesWithAvailabilities,
-  selectedDayCallback,
-  currentMonthCallback
-}) => {
-  const icon = require('./kBlue.png')
-  const [currentSelectedDate, setCurrentSelectedDate] = useState(new Date())
-  const [customDatesStyles, setCustomDatesStyles] = useState<CustomDateStyle[]>(
-    []
-  )
-  const [selectedMonthYear, setSelectedMonthYear] = useState(
-    moment().startOf('month')
-  )
-
-  const dateContainsAvailability = (
-    day: moment.Moment,
-    arr: CustomDateStyle[]
-  ) => {
-    let dayHasAvailability = false
-    datesWithAvailabilities.forEach((element: DateWithAvailability) => {
-      let avDate = element.date.clone()
-      avDate = avDate.startOf('day')
-
-      if (
-        day.startOf('day').isSame(avDate, 'day') &&
-        element.availabilities.length > 0
-      ) {
-        arr.push({
-          date: day.clone(),
-          style: {
-            backgroundColor: Colors.blue.color
-          },
-          textStyle: {
-            color: '#FFFFFF'
-          } // sets the font color
-        })
-        dayHasAvailability = true
-      }
-    })
-
-    if (!dayHasAvailability) {
-      arr.push({
-        date: day.clone(),
-        style: {},
-        textStyle: {} // sets the font color
-      })
-    }
-  }
-  useEffect(() => {}, [selectedMonthYear])
-  useEffect(() => {
-    const tempMonth = selectedMonthYear.clone()
-
-    changeCustomDatesStyle(tempMonth)
-  }, [datesWithAvailabilities])
-
-  const changeCustomDatesStyle = (date: moment.Moment) => {
-    const startOfMonth = date.clone()
-    const day = date
-    const styles: CustomDateStyle[] = []
-    while (day.add(1, 'day').isSame(startOfMonth, 'month')) {
-      dateContainsAvailability(day.clone(), styles)
-    }
-    setCustomDatesStyles(styles)
-  }
-
-  const customDayHeaderStyles: CustomDayHeaderStylesFunc = ({}) => {
-    return {
-      textStyle: {
-        color: textStyles.blackSerifHeader4.color,
-        fontFamily: textStyles.blackSerifHeader4.fontFamily,
-        fontSize: textStyles.blackSerifHeader4.fontSize
-      }
-    }
-  }
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={icon} />
-      </View>
-      <FadeInView duration={1000} style={undefined}>
-        <CalendarPicker
-          startFromMonday
-          previousComponent={
-            <ChevronLeftIcon size={20} fill={Colors.red.color} />
-          }
-          nextComponent={<ChevronRightIcon size={20} fill={Colors.red.color} />}
-          dayLabelsWrapper={styles.dayLabel}
-          monthTitleStyle={styles.month}
-          yearTitleStyle={styles.year}
-          // scrollable={true}
-          selectMonthTitle="Select a Month in "
-          headerWrapperStyle={styles.header}
-          todayBackgroundColor={'rgba(221, 38, 56, 0.5)'}
-          todayTextStyle={styles.TodayText}
-          selectedDayColor={Colors.red.color}
-          selectedDayTextStyle={styles.selectedText}
-          selectedDayStyle={{
-            backgroundColor: Colors.red.color,
-            transform: [{ scale: 1 }]
-          }}
-          showDayStragglers
-          headingLevel={3}
-          customDayHeaderStyles={customDayHeaderStyles}
-          onDateChange={(date: Moment) => {
-            selectedDayCallback(date)
-          }}
-          customDatesStyles={customDatesStyles}
-          onMonthChange={(currentMonthYear: moment.Moment) => {
-            setSelectedMonthYear(currentMonthYear)
-            changeCustomDatesStyle(selectedMonthYear)
-            currentMonthCallback(currentMonthYear)
-          }}
-          selectedStartDate={currentSelectedDate}
-          textStyle={{ fontFamily: textStyles.blackSerifBody1.fontFamily }}
-          minDate={moment().startOf('year').toDate()}
-        />
-      </FadeInView>
-    </View>
-  )
-}
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 20
@@ -175,11 +49,150 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     position: 'absolute',
     opacity: 0.1
-  },
-  logo: {
-    width: 219.81,
-    height: 205
   }
 })
+
+export type Props = {
+  datesWithAvailabilities: DateWithAvailability[]
+  selectedDayCallback: (date: Moment) => void
+  currentMonthCallback: (month: Moment) => void
+}
+
+const ActualCalendar: React.FC<Props> = ({
+  datesWithAvailabilities,
+  selectedDayCallback,
+  currentMonthCallback
+}) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [startSelectedDate, setSelectedStartDate] = useState(new Date())
+  const [customDatesStyles, setCustomDatesStyles] = useState<CustomDateStyle[]>(
+    []
+  )
+  const [selectedMonthYear, setSelectedMonthYear] = useState(
+    moment().startOf('month')
+  )
+
+  const dateContainsAvailability = useCallback(
+    (day: moment.Moment, arr: CustomDateStyle[]) => {
+      let dayHasAvailability = false
+      datesWithAvailabilities.forEach((element: DateWithAvailability) => {
+        let avDate = element.date.clone()
+        avDate = avDate.startOf('day')
+
+        if (
+          day.startOf('day').isSame(avDate, 'day') &&
+          element.availabilities.length > 0
+        ) {
+          arr.push({
+            date: day.clone(),
+            style: {
+              backgroundColor: Colors.blue.color
+            },
+            textStyle: {
+              color: '#FFFFFF'
+            }
+          })
+          dayHasAvailability = true
+        }
+      })
+
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      if (!dayHasAvailability) {
+        arr.push({
+          date: day.clone(),
+          style: {},
+          textStyle: {} // sets the font color
+        })
+      }
+    },
+    [datesWithAvailabilities]
+  )
+  const changeCustomDatesStyle = useCallback(
+    (date: moment.Moment) => {
+      const startOfMonth = date.clone()
+      const day = date
+      const dateStyles: CustomDateStyle[] = []
+      while (day.add(1, 'day').isSame(startOfMonth, 'month')) {
+        dateContainsAvailability(day.clone(), dateStyles)
+      }
+      setCustomDatesStyles(dateStyles)
+    },
+    [dateContainsAvailability]
+  )
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  useEffect(() => {}, [selectedMonthYear])
+  useEffect(() => {
+    const tempMonth = selectedMonthYear.clone()
+
+    changeCustomDatesStyle(tempMonth)
+  }, [changeCustomDatesStyle, datesWithAvailabilities, selectedMonthYear])
+
+  const customDayHeaderStyles: CustomDayHeaderStylesFunc = () => {
+    return {
+      textStyle: {
+        color: textStyles.blackSerifHeader4.color,
+        fontFamily: textStyles.blackSerifHeader4.fontFamily,
+        fontSize: textStyles.blackSerifHeader4.fontSize
+      }
+    }
+  }
+
+  const onDateChange = useCallback(
+    (date: Moment) => {
+      selectedDayCallback(date)
+    },
+    [selectedDayCallback]
+  )
+
+  const onMonthChange = useCallback(
+    (currentMonthYear: moment.Moment) => {
+      setSelectedMonthYear(currentMonthYear)
+      changeCustomDatesStyle(selectedMonthYear)
+      currentMonthCallback(currentMonthYear)
+    },
+    [changeCustomDatesStyle, currentMonthCallback, selectedMonthYear]
+  )
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Logo width={310} height={270} />
+      </View>
+      <FadeInView duration={1000} style={undefined}>
+        <CalendarPicker
+          startFromMonday
+          previousComponent={
+            <ChevronLeftIcon size={20} fill={Colors.red.color} />
+          }
+          nextComponent={<ChevronRightIcon size={20} fill={Colors.red.color} />}
+          dayLabelsWrapper={styles.dayLabel}
+          monthTitleStyle={styles.month}
+          yearTitleStyle={styles.year}
+          // scrollable={true}
+          selectMonthTitle={'Select a Month in '}
+          headerWrapperStyle={styles.header}
+          todayBackgroundColor={'rgba(221, 38, 56, 0.5)'}
+          todayTextStyle={styles.TodayText}
+          selectedDayColor={Colors.red.color}
+          selectedDayTextStyle={styles.selectedText}
+          selectedDayStyle={{
+            backgroundColor: Colors.red.color,
+            transform: [{ scale: 1 }]
+          }}
+          showDayStragglers
+          headingLevel={3}
+          // eslint-disable-next-line react/jsx-no-bind
+          customDayHeaderStyles={customDayHeaderStyles}
+          onDateChange={onDateChange}
+          customDatesStyles={customDatesStyles}
+          onMonthChange={onMonthChange}
+          selectedStartDate={startSelectedDate}
+          textStyle={{ fontFamily: textStyles.blackSerifBody1.fontFamily }}
+          minDate={moment().startOf('year').toDate()}
+        />
+      </FadeInView>
+    </View>
+  )
+}
 
 export default ActualCalendar
